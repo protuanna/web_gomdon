dce16bb1805f7d2ee285050fa2a7ff01a068aaf5
tuanna <tuannguyenanh@vccorp.vn>
2022-09-25 08:22:31 +0700

    up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -53,6 +53,13 @@ export const authOptions = {
             session.user = token.user
             session.accessToken = token.accessToken
             return session
+        },
+        async redirect({ url, baseUrl }) {
+            // Allows relative callback URLs
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            // Allows callback URLs on the same origin
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
         }
     }
 }
